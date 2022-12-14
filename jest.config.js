59c1ba8f34e9204063bea3d80c09a6a85ef7b87c
Davi de Medeiros <davi.demedeiros@here.com>
2022-12-14 10:51:14 -0300

    add initial jest config

---
+++ jest.config.js
@@ -0,0 +1,15 @@
+export default {
+  preset: 'ts-jest',
+  coverageDirectory: './coverage',
+  coveragePathIgnorePatterns: ['node_modules'],
+  moduleNameMapper: {
+    '\\.css$': '<rootDir>/src/mocks/styleMock.ts',
+  },
+  reporters: ['default'],
+  testEnvironment: 'jsdom',
+  testMatch: ['<rootDir>/src/**/*.test.{ts,tsx}'],
+  testPathIgnorePatterns: ['/node_modules/'],
+  transform: {
+    '/.ts/': ['ts-jest', { diagnostics: false }],
+  },
+};
