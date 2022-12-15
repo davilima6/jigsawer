4f6cf559590d8ad2b9a78db57a168e31b45d0810
Davi de Medeiros <davi.demedeiros@here.com>
2022-12-15 16:07:13 -0300

    initial implementation

---
+++ src/components/Loading/Loading.tsx
@@ -0,0 +1,7 @@
+import './Loading.css';
+
+function Loading() {
+  return <div>Loading&hellip;</div>;
+}
+
+export default Loading;
--- src/views/App/App.tsx
+++ src/views/App/App.tsx
@@ -1,7 +1,28 @@
-import './App.css'
+import { useState } from 'react';
+import Challenge from '../../components/Challenge';
+import Status from '../../components/Status';
+import './App.css';
+
+const INITIAL_CHANCES_AMOUNT = 3;
 
 function App() {
-  return null
+  const [points, setPoints] = useState(0);
+  const [chancesLeft, setChancesLeft] = useState(INITIAL_CHANCES_AMOUNT);
+
+  function handleHit() {
+    setPoints((pointsCur) => pointsCur + 1);
+  }
+
+  function handleMiss() {
+    setChancesLeft((chancesLeftCur) => chancesLeftCur - 1);
+  }
+
+  return (
+    <>
+      <Challenge onHit={handleHit} onMiss={handleMiss} />
+      <Status chancesLeft={chancesLeft} points={points} />
+    </>
+  );
 }
 
-export default App
+export default App;
