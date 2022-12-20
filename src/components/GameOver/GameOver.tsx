a4bf2117b0cc783859080dfbe7441f0aaeed80c4
Davi de Medeiros <davi.demedeiros@here.com>
2022-12-20 14:10:50 -0300

    refactor Answer component to have a single responsiblity

--- src/components/GameOver/GameOver.tsx
+++ src/components/GameOver/GameOver.tsx
@@ -1,17 +1,23 @@
+import './GameOver.css';
+
 type Props = {
+  onGameOver: () => void;
   score: number;
 };
 
-function GameOver({ score }: Props) {
+function GameOver({ onGameOver, score }: Props) {
   const compliment = score > 0 ? 'Congratulations' : 'Pity';
   const needsPlural = score === 0 || score > 1;
 
   return (
-    <section>
+    <section className="game-over-wrapper">
       <h1>Game over.</h1>
       <p>
         {compliment}, you made {score} point{needsPlural ? 's' : ''}!
       </p>
+      <button className="reset" onClick={onGameOver} autoFocus>
+        Try again
+      </button>
     </section>
   );
 }
