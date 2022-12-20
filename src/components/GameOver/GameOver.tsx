2c7ba304c9142ae332af1acc481267e49cc28bda
Davi de Medeiros <davi.demedeiros@here.com>
2022-12-20 13:42:04 -0300

    include achieved score in GameOver message

--- src/components/GameOver/GameOver.tsx
+++ src/components/GameOver/GameOver.tsx
@@ -1,6 +1,18 @@
-function GameOver() {
+type Props = {
+  score: number;
+};
+
+function GameOver({ score }: Props) {
+  const compliment = score > 0 ? 'Congratulations' : 'Pity';
+  const needsPlural = score === 0 || score > 1;
+
   return (
-    <h1>Game over.</h1>
+    <section>
+      <h1>Game over.</h1>
+      <p>
+        {compliment}, you made {score} point{needsPlural ? 's' : ''}!
+      </p>
+    </section>
   );
 }
 
